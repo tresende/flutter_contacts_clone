df350ebd7d03b5ae5ef125b850a1f6a9bf3714e9
tresende <thiago.gcresende@gmail.com>
2019-03-24 09:13:50 -0300

    Card do contato

--- lib/ui/home_page.dart
+++ lib/ui/home_page.dart
@@ -1,3 +1,5 @@
+import 'dart:io';
+
 import 'package:flutter/material.dart';
 import 'package:flutter_contacts_clone/helpers/contact_helper.dart';
 
@@ -8,21 +10,24 @@ class HomePage extends StatefulWidget {
 
 class _HomePageState extends State<HomePage> {
   ContactHelper helper = ContactHelper();
+  List<Contact> contacts = List<Contact>();
 
-  // @override
-  // void initState() {
-  //   super.initState();
-  //   Contact contact = Contact();
-  //   contact.name = 'Thiago';
-  //   contact.email = 'thiago.gcresende@gmail.com';
-  //   contact.phone = '+55(99)9-9999-9999';
-  //   contact.img = 'teste.png';
-  //   this.helper.saveContact(contact);
+  @override
+  void initState() {
+    super.initState();
+    // Contact contact = Contact();
+    // contact.name = 'Thiago';
+    // contact.email = 'thiago.gcresende@gmail.com';
+    // contact.phone = '+55(99)9-9999-9999';
+    // contact.img = 'teste.png';
+    // this.helper.saveContact(contact);
 
-  //   helper.getAllContacts().then((list) {
-  //     print(list);
-  //   });
-  // }
+    helper.getAllContacts().then((list) {
+      setState(() {
+        this.contacts = list;
+      });
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -37,6 +42,36 @@ class _HomePageState extends State<HomePage> {
         backgroundColor: Colors.red,
         child: Icon(Icons.add),
       ),
+      body: ListView.builder(
+        itemBuilder: (context, index) {
+          return contactCard(context, index);
+        },
+        itemCount: this.contacts.length,
+        padding: EdgeInsets.all(10),
+      ),
     );
+  }
+
+  Widget contactCard(BuildContext context, int index) {
+    Contact contact = contacts[index];
+    return GestureDetector(
+        child: Card(
+            child: Padding(
+      child: Row(
+        children: <Widget>[
+          Container(
+            width: 80,
+            height: 80,
+            decoration: BoxDecoration(
+                shape: BoxShape.circle,
+                image: DecorationImage(
+                    image: contact.img != null
+                        ? FileImage(File(contact.img))
+                        : AssetImage('images/person.png'))),
+          )
+        ],
+      ),
+      padding: EdgeInsets.all(10),
+    )));
   }
 }
